8044b1e0f02c33bc2d51171326a6d855700ca608
Hampus Hallkvist <hampus@hallkvist.org>
2022-06-10 21:24:30 +0200

    Remove validation from execute_move This is done separately using the validation file

--- src/game/execute_move.rs
+++ src/game/execute_move.rs
@@ -2,7 +2,7 @@ use super::validation::tile_occupied;
 use super::game::{Move, Wall};
 use super::player::Player;
 
-pub fn execute_move(walls: &mut Vec<Wall>, active_player: &mut Player, other_player: &Player, player_move: &Move) {
+pub fn execute_move(walls: &mut Vec<Wall>, active_player: &mut Player, player_move: &Move) {
 	match &*player_move {
 		Move::Wall(wall) => {
 			if active_player.wall_count <= 0 {
@@ -14,12 +14,6 @@ pub fn execute_move(walls: &mut Vec<Wall>, active_player: &mut Player, other_pla
 		},
 		other => {
 			let (new_x, new_y) = active_player.move_player(other);
-			
-			// If the tile is occupied we do not allow the player to move
-			if tile_occupied(walls, active_player, other_player, new_x, new_y, false) {
-				return;
-			}
-
 			active_player.set_new_coordinates(new_x, new_y);
 		}
 	}
