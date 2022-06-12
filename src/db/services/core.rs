a57ae0cf62903b540d3546ad871c50cd368f5bc8
Hampus Hallkvist <hampus@hallkvist.org>
2022-06-12 12:30:13 +0200

    Add first iteration of match_maker

--- src/db/services/core.rs
+++ src/db/services/core.rs
@@ -2,6 +2,7 @@ use crate::db::create_issue_comment::create_issue_comment;
 use crate::db::models::submission_model::Submission;
 use crate::db::models::user_model::User;
 use crate::db::services::webhook_schema::GithubPayload;
+use crate::db::services::match_maker::match_make;
 use crate::db::db::DbPool;
 use actix_web::{HttpResponse, web};
 use actix_web::{post, Error};
@@ -24,13 +25,12 @@ pub async fn submit_challenge(
 	}
 
 	if user.is_none() {
+		create_issue_comment(webhook_post.issue.number, "Internal error");
 		return Ok(HttpResponse::Ok().body("Could not find or create user..."));
 	}
 
-	let result = Submission::list(&conn);
-
 	// Create submission
-	Submission::create(
+	let challenger = Submission::create(
 		&user.unwrap().id, 
 		&webhook_post.issue.body, 
 		Some(&webhook_post.issue.title), 
@@ -40,7 +40,27 @@ pub async fn submit_challenge(
 		&conn
 	);
 
-	create_issue_comment(webhook_post.issue.number, "Thanks for submitting!<br>Your code is being processed...");
+	if challenger.is_none() {
+		create_issue_comment(webhook_post.issue.number, "Could not create submission...<br>Try again later");
+		return Ok(HttpResponse::Ok().body("Could not create submission..."));
+	}
 
-	Ok(HttpResponse::Ok().json("Challenge has started for bot"))
+	create_issue_comment(webhook_post.issue.number, format!("User: {}<br>Script-id: {}<br>Thanks for submitting!<br>Your code is being processed...", webhook_post.sender.login, challenger.unwrap().id));
+
+	let errors = match_make(&challenger.unwrap(), &conn);
+
+	let mut output = String::new();
+	
+	if errors.len() > 0 {
+		for error in errors {
+			output += &error;
+			output += "<br>";
+		}
+		create_issue_comment(webhook_post.issue.number, &output);
+	} else {
+		create_issue_comment(webhook_post.issue.number, "Matches performed!");
+	}
+
+
+	Ok(HttpResponse::Ok().json("Successfully submitted challenge"))
 }
--- src/db/services/match_maker.rs
+++ src/db/services/match_maker.rs
@@ -1,14 +1,69 @@
+use crate::game::game::GameState;
+use crate::game::entry_point::initialize_game_session;
 use diesel::SqliteConnection;
 use crate::db::models::submission_model::Submission;
-use crate::db::schema::Submissions::dsl::Submissions as submission_dsl;
+use crate::db::models::match_model::Match;
 
-pub struct Challenger {
-	pub script: String,
-	pub wins: u32,
+pub fn match_make(challenger: &Submission, conn: &SqliteConnection) -> Vec<String> {
+	// Order by score and pick the submission with the higest score
+	let submissions = Submission::list(conn);
+	let mut matches = make_selection(submissions);
+	
+	let mut new_challenger = challenger.clone();
+
+	let mut errors: Vec<String> = vec![];
+
+	// Match-maker goes here
+	for i in 0..matches.len() {
+		let result = initialize_game_session(&challenger.script, &matches[i].script);
+		let winner: Option<String>;
+		let loser: Option<String>;
+		match result {
+			Ok(GameState::PlayerOneWon) => {
+				new_challenger.score += 1;
+				winner = Some(challenger.id.clone());
+				loser = Some(matches[i].id.clone());
+			},
+			Ok(GameState::PlayerTwoWon) => {
+				matches[i].score += 1;
+				winner = Some(matches[i].id.clone());
+				loser = Some(challenger.id.clone());
+			},
+			Ok(_) => {
+				errors.push(format!("[Match {}] Invalid game state", i));
+				continue;
+			},
+			Err(error) => {
+				errors.push(format!("[Match {}] Error: {}", i, error));
+				continue
+			},
+		}
+
+		matches[i].save(conn);
+		Match::create(&winner.unwrap(), &loser.unwrap(), conn);
+	}
+
+	new_challenger.save(conn);
+
+	return errors;
 }
 
-fn match_make(challenger: &mut Challenger, conn: &SqliteConnection) {
-	// Order by score and pick the submission with the higest score
-	
-	// Match-maker goes here
+fn make_selection(submissions: Vec<Submission>) -> Vec<Submission> {
+	let mut submissions = submissions;
+	let mut match_queue: Vec<Submission> = Vec::new();
+
+	// Sort from lowest to highest
+	submissions.sort_by(|a, b| a.score.cmp(&b.score));
+
+	if submissions.len() < 10 {
+		return submissions;
+	}
+
+	// Only pick out then submissions, equally spread
+	for i in 0..10 {
+		let index = i * submissions .len() / 10;
+		match_queue.push(submissions[index].clone());
+	}
+
+	return match_queue;
 }
