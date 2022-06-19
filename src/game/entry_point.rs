e512765fc6aa5d2382a87b817c6de3e04f966cf2
Hampus Hallkvist <hampus@hallkvist.org>
2022-06-19 19:52:46 +0200

    Rename variable app to game_session

--- src/game/entry_point.rs
+++ src/game/entry_point.rs
@@ -4,8 +4,8 @@ pub fn initialize_game_session(script_1: &str, script_2: &str) -> Result<GameSta
     let std = std::fs::read_to_string("/Users/hampfh/dev/Projects/rust/hahalang/scripts/std.lua")
         .expect("Could not load standard library");
 
-    let mut app = Game::new(std);
-    match app.start(script_1.to_string(), script_2.to_string()) {
+    let mut game_session = Game::new(std);
+    match game_session.start(script_1.to_string(), script_2.to_string()) {
         Ok(GameState::PlayerOneWon) => Ok(GameState::PlayerOneWon),
         Ok(GameState::PlayerTwoWon) => Ok(GameState::PlayerTwoWon),
         Ok(game_state) => Err(format!("Invalid game state {:?}", game_state)),
