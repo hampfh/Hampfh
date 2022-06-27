5d3696d122314d1a4fe70c68ea8ba2262d961e4f
Hampus Hallkvist <hampus@hallkvist.org>
2022-06-27 20:57:19 +0200

    Format file

--- src/game/execute_move.rs
+++ src/game/execute_move.rs
@@ -1,19 +1,26 @@
-use super::game::{Move, Wall};
+use super::game::{ErrorType, Move, Wall};
 use super::player::Player;
 
-pub fn execute_move(walls: &mut Vec<Wall>, active_player: &mut Player, player_move: &Move) {
-	match &*player_move {
-		Move::Wall(wall) => {
-			if active_player.wall_count <= 0 {
-				return;
-			}
-			
-			active_player.decrement_wall_count();
-			walls.push(wall.clone());
-		},
-		other => {
-			let (new_x, new_y) = active_player.move_player(other);
-			active_player.set_new_coordinates(new_x, new_y);
-		}
-	}
+pub fn execute_move(
+    walls: &mut Vec<Wall>,
+    active_player: &mut Player,
+    player_move: &Move,
+) -> Result<(), ErrorType> {
+    match &*player_move {
+        Move::Wall(wall) => {
+            if active_player.wall_count <= 0 {
+                return Err(ErrorType::GameError {
+                    reason: format!("No more walls to place, all walls already used"),
+                });
+            }
+
+            active_player.decrement_wall_count();
+            walls.push(wall.clone());
+        }
+        other => {
+            let (new_x, new_y) = active_player.move_player(other);
+            active_player.set_new_coordinates(new_x, new_y);
+        }
+    }
+    Ok(())
 }
