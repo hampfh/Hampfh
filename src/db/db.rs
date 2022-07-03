63ca36658c1617b56c629f55aa5775d96d4e5d17
Hampus Hallkvist <hampus@hallkvist.org>
2022-07-03 02:02:40 +0200

    If no database exists we create one

--- src/db/db.rs
+++ src/db/db.rs
@@ -1,3 +1,5 @@
+use std::path::Path;
+
 use diesel::prelude::*;
 use diesel::r2d2::{self, ConnectionManager};
 
@@ -10,9 +12,19 @@ pub type DbPool = r2d2::Pool<ConnectionManager<SqliteConnection>>;
 pub fn establish_connection() -> DbPool {
     dotenv().ok();
 
+    // If no database exists we create it.
+    let path_string =
+        std::env::var("DATABASE_URL").expect("NO DATABASE_URL specified in .env file");
+    let path = Path::new(&path_string);
+
+    if !path.exists() {
+        let prefix = path.parent().unwrap();
+        std::fs::create_dir_all(prefix).unwrap();
+        std::fs::File::create(&path_string).unwrap();
+    }
+
     // set up database connection pool
-    let conn_spec = std::env::var("DATABASE_URL").expect("DATABASE_URL");
-    let manager = ConnectionManager::<SqliteConnection>::new(conn_spec);
+    let manager = ConnectionManager::<SqliteConnection>::new(path_string);
     return r2d2::Pool::builder()
         .build(manager)
         .expect("Failed to create pool.");
