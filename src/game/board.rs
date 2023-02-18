007ac6a508993728983724675d5d619a53de5986
Hampus Hallkvist <hampus@hallkvist.org>
2023-02-18 15:04:49 +0100

    fix: Add tests for board.rs

--- src/game/board.rs
+++ src/game/board.rs
@@ -2,7 +2,7 @@ use super::game::Wall;
 use super::game::MAP_SIZE;
 use crate::game::player::Player;
 
-#[derive(std::fmt::Debug, Copy, Clone)]
+#[derive(std::fmt::Debug, Copy, Clone, PartialEq, Eq)]
 pub enum Tile {
     Empty = 0,
     P1 = 1,
@@ -74,4 +74,109 @@ pub fn board_from_string(board: String) -> Vec<Tile> {
         };
     }
     return output;
+}
+
+#[cfg(test)]
+mod tests {
+
+    use crate::game::{
+        board::board_to_string,
+        game::{Wall, MAP_SIZE},
+        player::{Player, PlayerType},
+    };
+
+    use super::{board_from_string, create_empty_board, place_tile, Tile};
+
+    #[test]
+    fn populate_board_test() {
+        let p1 = Player::new(4, 4, 10, PlayerType::Flipped);
+        let p2 = Player::new(8, 8, 10, PlayerType::Regular);
+        // Create walls
+        let mut walls: Vec<Wall> = Vec::new();
+        for i in 0..MAP_SIZE {
+            walls.push(Wall {
+                x1: i,
+                y1: 0,
+                x2: i,
+                y2: 1,
+            });
+            walls.push(Wall {
+                x1: i,
+                y1: MAP_SIZE - 1,
+                x2: i,
+                y2: MAP_SIZE - 2,
+            });
+            walls.push(Wall {
+                x1: 0,
+                y1: i,
+                x2: 1,
+                y2: i,
+            });
+            walls.push(Wall {
+                x1: MAP_SIZE - 1,
+                y1: i,
+                x2: MAP_SIZE - 2,
+                y2: i,
+            });
+        }
+
+        // Create expected board
+        let mut expected_board = create_empty_board();
+        for wall in walls.clone() {
+            place_tile(&mut expected_board, wall.x1, wall.y1, Tile::Wall);
+            place_tile(&mut expected_board, wall.x2, wall.y2, Tile::Wall);
+        }
+        place_tile(&mut expected_board, p1.x, p1.y, Tile::P1);
+        place_tile(&mut expected_board, p2.x, p2.y, Tile::P2);
+
+        let board = super::populate_board(&p1, &p2, &walls);
+        assert_eq!(board, expected_board);
+    }
+
+    #[test]
+    fn create_empty_board_test() {
+        let board = create_empty_board();
+        for tile in board.iter() {
+            assert_eq!(*tile, Tile::Empty);
+        }
+    }
+
+    #[test]
+    fn place_tile_test() {
+        let mut board = create_empty_board();
+
+        place_tile(&mut board, 0, 0, Tile::P1);
+        assert_eq!(board[0], Tile::P1);
+
+        place_tile(&mut board, MAP_SIZE - 1, MAP_SIZE - 1, Tile::P1);
+        assert_eq!(board[(MAP_SIZE * MAP_SIZE - 1) as usize], Tile::P1);
+
+        place_tile(&mut board, 0, 0, Tile::P2);
+        assert_eq!(board[0], Tile::P2);
+
+        place_tile(&mut board, MAP_SIZE - 1, 0, Tile::Wall);
+        place_tile(&mut board, MAP_SIZE - 2, 0, Tile::Wall);
+        assert_eq!(board[(MAP_SIZE - 1) as usize], Tile::Wall);
+        assert_eq!(board[(MAP_SIZE - 2) as usize], Tile::Wall);
+    }
+
+    #[test]
+    fn board_conversions_test() {
+        let mut board_string = String::new();
+        let mut board: Vec<Tile> = Vec::new();
+        for i in 0..(MAP_SIZE * MAP_SIZE) {
+            let (tile, tile_string) = match i % 4 {
+                0 => (Tile::Empty, "0"),
+                1 => (Tile::P1, "1"),
+                2 => (Tile::P2, "2"),
+                3 => (Tile::Wall, "3"),
+                num => panic!("Invalid tile {}", num),
+            };
+            board_string.push_str(tile_string);
+            board.push(tile);
+        }
+
+        assert_eq!(board.clone(), board_from_string(board_string.clone()));
+        assert_eq!(board_string, board_to_string(board));
+    }
 }
