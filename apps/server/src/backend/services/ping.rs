904f840b4c40d7b6574aceac9bc9be094b289f69
Hampus Hallkvist <hampus@hallkvist.org>
2024-09-30 22:56:33 +0200

    fix: Remove unused dependency rlua

--- apps/server/src/backend/services/ping.rs
+++ apps/server/src/backend/services/ping.rs
@@ -1,13 +1,4 @@
-use std::path::PathBuf;
-
-use actix_files::NamedFile;
 use actix_web::get;
-
-#[get("/")]
-pub(super) async fn get_ping() -> actix_web::Result<NamedFile> {
-    let path: PathBuf = "./static/index.html".parse().unwrap();
-    return Ok(NamedFile::open(path).unwrap());
-}
 
 #[get("/api/ping")]
 pub(super) async fn get_api_ping() -> actix_web::Result<String> {
--- apps/server/src/backend/services/routes.rs
+++ apps/server/src/backend/services/routes.rs
@@ -1,15 +1,12 @@
 use actix_web::web;
 
 use super::{
-    core::submit_challenge,
-    match_data::get_match_route,
-    matches::get_matches_route,
-    ping::{get_api_ping, get_ping},
+    core::submit_challenge, match_data::get_match_route, matches::get_matches_route,
+    ping::get_api_ping,
 };
 
 pub(crate) fn routes(cfg: &mut web::ServiceConfig) {
     cfg.service(submit_challenge);
-    cfg.service(get_ping);
     cfg.service(get_api_ping);
     cfg.service(get_match_route);
     cfg.service(get_matches_route);
